ff8a9f9a02878569a615dfb2e9c476fd0cfdf287
Hyun <chomman@naver.com>
2014-06-30 10:55:22 +0900

    log4j properties configuration tutorial

---
+++ log4j-tutorial/2-propertiest-setup/src/main/java/com/haeny/tutorial/log4j/PropertiesSettingTest.java
@@ -0,0 +1,12 @@
+package com.haeny.tutorial.log4j;
+
+import org.apache.log4j.Logger;
+
+public class PropertiesSettingTest {
+	
+	static final Logger logger = Logger.getLogger(PropertiesSettingTest.class);
+
+	public static void main(String[] args) {
+		 logger.debug("Log4j Propertiest configuration success.");
+	}
+}
